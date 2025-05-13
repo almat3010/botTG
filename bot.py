d93708c57ddc68cb00008f8ea73135ed89d1f60b
Султанов Алмат <almat.sultanov@t2.ru>
2025-05-14 00:54:56 +0300

    add options

--- bot.py
+++ bot.py
@@ -23,10 +23,20 @@ async def get_countdown_text():
         options.headless = True
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--disable-blink-features=AutomationControlled")
+        options.add_argument("--disable-infobars")
+        options.add_argument("--disable-extensions")
+        options.add_argument("--start-maximized")
+        options.add_argument("--window-size=1920,1080")
+        options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                            "AppleWebKit/537.36 (KHTML, like Gecko) "
+                            "Chrome/113.0.0.0 Safari/537.36")
         options.binary_location = "/usr/bin/google-chrome"
+
 
         driver = uc.Chrome(options=options)
         driver.get("https://case-battle.at/case/awpasiimov")
+        driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
         driver.implicitly_wait(15)
 
         element = driver.find_element(By.CSS_SELECTOR, "#case-box-app > div.countdown > div:nth-child(3)")
